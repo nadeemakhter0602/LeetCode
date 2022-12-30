b6779cc321e9c3bd319800aa453c3dbb67cd8f29
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2022-12-30 18:45:51 +0530

    add Binary Tree Postorder Traversal recursive

---
+++ Binary Tree Postorder Traversal/solution.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        def post_order(root, res):
+            if root is None:
+                return
+            post_order(root.left, res)
+            post_order(root.right, res)
+            res.append(root.val)
+        res = []
+        post_order(root, res)
+        return res
