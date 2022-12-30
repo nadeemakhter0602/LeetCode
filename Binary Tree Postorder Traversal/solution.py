b8489326edb26ccf5f871d7c89dba5db9047e572
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2022-12-30 20:10:15 +0530

    add binary tree post order traversal iterative

--- Binary Tree Postorder Traversal/solution.py
+++ Binary Tree Postorder Traversal/solution.py
@@ -15,3 +15,15 @@ class Solution:
         res = []
         post_order(root, res)
         return res
+
+    def postorderTraversalIterative(self, root: Optional[TreeNode]) -> List[int]:
+        stack = [root]
+        res = []
+        while len(stack) > 0 and root is not None:
+            curr = stack.pop()
+            res.append(curr.val)
+            if curr.left is not None:
+                stack.append(curr.left)
+            if curr.right is not None:
+                stack.append(curr.right)
+        return res[::-1]
