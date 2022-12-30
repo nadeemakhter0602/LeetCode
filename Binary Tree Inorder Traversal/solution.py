1e3229da70ee14bf3d9978bbf5bc4a9339215ff2
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2022-12-30 18:05:26 +0530

    add Binary Tree Inorder Traversal Iterative solution

--- Binary Tree Inorder Traversal/solution.py
+++ Binary Tree Inorder Traversal/solution.py
@@ -15,3 +15,17 @@ class Solution:
         res = []
         inorder(root, res)
         return res
+
+    # iterative solution
+    def inorderTraversalIterative(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        stack = []
+        curr = root
+        while curr is not None or len(stack) > 0:
+            while curr is not None:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            res.append(curr.val)
+            curr = curr.right
+        return res
