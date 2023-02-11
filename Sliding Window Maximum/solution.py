0887b2f31ad372de961609c20968bb90b7e50d30
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-02-11 23:12:26 +0530

    remove use of window list

--- Sliding Window Maximum/solution.py
+++ Sliding Window Maximum/solution.py
@@ -2,19 +2,16 @@ from collections import deque
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        window = deque()
         decreasing = deque()
         s = []
         for i in range(len(nums)):
             while len(decreasing) > 0 and decreasing[-1] < nums[i]:
                 decreasing.pop()
             decreasing.append(nums[i])
-            window.append(nums[i])
-            if len(window) == k:
-                if decreasing[0] == window[0]:
+            if i - k + 1 >= 0:
+                if decreasing[0] == nums[i - k + 1]:
                     max_ = decreasing.popleft()
                     s.append(max_)
                 else:
                     s.append(decreasing[0])
-                window.popleft()
         return s
