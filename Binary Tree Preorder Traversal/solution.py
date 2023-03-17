1ba31e9f350ba13607cb2915ec76d6a800c145b1
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-03-17 20:15:30 +0530

    add Binary Tree Preorder Traversal Iterative

--- Binary Tree Preorder Traversal/solution.py
+++ Binary Tree Preorder Traversal/solution.py
@@ -15,3 +15,16 @@ class Solution:
         res = []
         preorder(root, res)
         return res
+
+    def preorderTraversalIterative(self, root: Optional[TreeNode]) -> List[int]:
+        stack = [root]
+        res = []
+        curr = root
+        while curr is not None and len(stack) > 0:
+            curr = stack.pop()
+            res.append(curr.val)
+            if curr.right is not None:
+                stack.append(curr.right)
+            if curr.left is not None:
+                stack.append(curr.left)
+        return res
