4593cb409ab0e7cc199a0327a749e4a124c4ab49
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-04-15 21:25:37 +0530

    remove use of negative index

--- Remove Duplicates from Sorted Array/solution.py
+++ Remove Duplicates from Sorted Array/solution.py
@@ -1,11 +1,14 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        k = 1
+        k = 0
         n = len(nums)
         for i in range(n):
-            if i + 1 < n and nums[i] != nums[i+1]:
-                nums[k - 1] = nums[i]
+            if i + 1 < n and nums[i] != nums[i + 1]:
+                nums[k] = nums[i]
                 k += 1
-        if nums[k - 2] != nums[-1]:
-            nums[k - 1] = nums[-1]
+        if k > 0 and nums[k - 1] != nums[n - 1]:
+            nums[k] = nums[n - 1]
+            k += 1
+        elif k == 0:
+            k = 1
         return k
