af5c242bf61b9286472e411e6ad2cd838f998b22
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-05-03 19:40:44 +0530

    using queue instead of stack

--- Minimum Depth of Binary Tree/solution.py
+++ Minimum Depth of Binary Tree/solution.py
@@ -1,3 +1,4 @@
+from collections import deque
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -8,17 +9,18 @@ class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        stack = [(root, 1)]
+        queue = deque()
+        queue.append((root, 1))
         m = float('inf')
-        while stack:
-            curr = stack.pop()
+        while queue:
+            curr = queue.popleft()
             distance = curr[1]
             curr = curr[0]
             if curr:
                 if curr.left:
-                    stack.append((curr.left, distance + 1))
+                    queue.append((curr.left, distance + 1))
                 if curr.right:
-                    stack.append((curr.right, distance + 1))
+                    queue.append((curr.right, distance + 1))
                 if not curr.left and not curr.right:
                     m = min(distance, m)
         return m
