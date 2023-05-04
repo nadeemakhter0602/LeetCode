b30f2f6295b0c0c252c6864a0fb2d27b34023e91
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-05-04 22:24:44 +0530

    using only one pass

--- Isomorphic Strings/solution.py
+++ Isomorphic Strings/solution.py
@@ -1,13 +1,14 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        def generate_uid(string):
-            mapping = dict()
-            uid = []
-            counter = 0
-            for i in string:
-                if i not in mapping:
-                    mapping[i] = counter
-                    counter += 1
-                uid.append(mapping[i])
-            return uid
-        return generate_uid(s) == generate_uid(t)
+        mapping_s = dict()
+        mapping_t = dict()
+        tmp_t = ""
+        tmp_s = ""
+        for i in range(len(s)):
+            if s[i] not in mapping_s:
+                mapping_s[s[i]] = t[i]
+            if t[i] not in mapping_t:
+                mapping_t[t[i]] = s[i]
+            tmp_t += mapping_s[s[i]]
+            tmp_s += mapping_t[t[i]]
+        return tmp_t == t and tmp_s == s
