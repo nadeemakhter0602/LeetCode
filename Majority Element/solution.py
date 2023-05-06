434a00caac4e3da3ee0663888239e40cc6db798f
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-05-06 14:00:22 +0530

    Majority Element using O(1) extra space

--- Majority Element/solution.py
+++ Majority Element/solution.py
@@ -1,7 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = dict()
-        for i in nums:
-            count[i] = 1 + count.get(i, 0)
-            if count[i] > len(nums) * 0.5:
-                return i
+        majority_element = nums[0]
+        count = 1
+        for i in range(len(nums)):
+            if nums[i] != majority_element:
+                count -= 1
+            else:
+                count += 1 
+            if count == 0:
+                majority_element = nums[i]
+                count = 1
+        return majority_element
