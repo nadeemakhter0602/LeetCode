db2373d87a51feb0573f1fc7483ec3f1c69b8302
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-05-11 18:12:41 +0530

    shorten lines of code

--- Reverse Bits/solution.py
+++ Reverse Bits/solution.py
@@ -2,10 +2,7 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         r = 0
         for i in range(31):
-            lsb = n & 1
-            r |= lsb
-            r = r << 1
+            r = (r | n & 1) << 1
             n = n >> 1
-        lsb = n & 1
-        r |= lsb
+        r |= n & 1
         return r
