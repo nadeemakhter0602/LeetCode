8a8e97be51e5208d016895f1fcbf2387b99553b4
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-05-12 20:07:04 +0530

    add solution without manipulating original number

--- Reverse Bits/solution.py
+++ Reverse Bits/solution.py
@@ -2,7 +2,9 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         r = 0
         for i in range(31):
-            r = (r | n & 1) << 1
-            n = n >> 1
-        r |= n & 1
+            if (n & 1 << i) > 0:
+                r |= 1
+            r <<= 1
+        if (n & 1 << 31) > 0:
+            r |= 1
         return r
