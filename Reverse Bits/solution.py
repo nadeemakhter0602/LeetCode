dc48f043353f169b58e87ce2031316f50ed51403
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-05-13 20:54:34 +0530

    no use of if statements

--- Reverse Bits/solution.py
+++ Reverse Bits/solution.py
@@ -2,9 +2,7 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         r = 0
         for i in range(31):
-            if (n & 1 << i) > 0:
-                r |= 1
+            r |= (n & 1 << i) > 0
             r <<= 1
-        if (n & 1 << 31) > 0:
-            r |= 1
+        r |= (n & 1 << 31) > 0
         return r
