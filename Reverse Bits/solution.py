0bc76a5840704881b102265b1aaa3d707eb4a855
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-05-13 21:41:51 +0530

    use swapping using bit manipulation

--- Reverse Bits/solution.py
+++ Reverse Bits/solution.py
@@ -1,8 +1,7 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         r = 0
-        for i in range(31):
-            r |= (n & 1 << i) > 0
-            r <<= 1
-        r |= (n & 1 << 31) > 0
+        for i in range(16):
+            r |= (n & 1 << i) << (31 - 2*i)
+            r |= (n & 1 << (31 - i)) >> (31 - 2*i)
         return r
