8375f49ae89a93b3ae099a8190dd5b6374147ed0
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-05-14 18:39:17 +0530

    Reverse bits no extra variable in place

--- Reverse Bits/solution.py
+++ Reverse Bits/solution.py
@@ -1,7 +1,10 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        r = 0
         for i in range(16):
-            r |= (n & 1 << i) << (31 - 2*i)
-            r |= (n & 1 << (31 - i)) >> (31 - 2*i)
-        return r
+            l = (n & 1 << i) << (31 - 2*i)
+            r = (n & 1 << (31 - i)) >> (31 - 2*i)
+            n &= ~ (1 << i)
+            n &= ~ (1 << (31 - i))
+            n |= l
+            n |= r
+        return n
