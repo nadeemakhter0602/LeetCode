cc2987b231efd1e20e4607f89c26ebef49838b4c
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-05-15 23:27:29 +0530

    adding solution with lookup table

--- Reverse Bits/solution.py
+++ Reverse Bits/solution.py
@@ -8,3 +8,25 @@ class Solution:
             n |= l
             n |= r
         return n
+
+
+'''
+# Creating lookup table for O(1) time complexity
+class Solution:
+    def __init__(self):
+        self.lookup = []
+        for j in range(65536):
+            n = j
+            for i in range(16):
+                l = (n & 1 << i) << (31 - 2 * i)
+                r = (n & 1 << (31 - i)) >> (31 - 2 * i)
+                n &= ~ (1 << i)
+                n &= ~ (1 << (31 - i))
+                n |= l
+                n |= r
+            self.lookup.append(n)
+
+    def reverseBits(self, n: int) -> int:
+        return self.lookup[n >> 16] >> 16 | self.lookup[n & 0xffff]
+
+'''
