d9edc2479da79e2b78359267bd3b8ba6cb3b4706
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2024-05-05 13:31:28 +0530

    add Coin Change II optimize space complexity

--- Coin Change II/solution.py
+++ Coin Change II/solution.py
@@ -1,13 +1,11 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [[0 for i in range(len(coins))] for i in range(amount + 1)]
-        for curr_amt in range(amount + 1):
-            for idx in range(len(coins)):
+        dp = [0 for i in range(amount + 1)]
+        for idx in range(len(coins)):
+            for curr_amt in range(amount + 1):
                 if curr_amt == 0:
-                    dp[curr_amt][idx] = 1
-                    continue
+                    dp[curr_amt] = 1
                 remaining = curr_amt - coins[len(coins) - 1 - idx]
                 if remaining >= 0:
-                    dp[curr_amt][idx] += dp[remaining][idx]
-                dp[curr_amt][idx] += dp[curr_amt][idx - 1] if idx - 1 >= 0 else 0
-        return dp[-1][-1]
+                    dp[curr_amt] += dp[remaining]
+        return dp[-1]
