533639ccf8515053d18dd44b113f03fe2c708db5
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2024-05-12 17:39:22 +0530

    store overlapping column max

--- Largest Local Values in a Matrix/solution.py
+++ Largest Local Values in a Matrix/solution.py
@@ -1,23 +1,13 @@
 class Solution:
     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        n = len(grid)
         maxLocal = []
-        for i in range(1, len(grid) - 1):
+        for i in range(n - 2):
             row = []
-            for j in range(1, len(grid) - 1):
-                max_center = float("-inf")
-                max_center = max(
-                    [
-                        grid[i][j],
-                        grid[i - 1][j - 1],
-                        grid[i + 1][j + 1],
-                        grid[i][j + 1],
-                        grid[i + 1][j],
-                        grid[i - 1][j],
-                        grid[i][j - 1],
-                        grid[i + 1][j - 1],
-                        grid[i - 1][j + 1],
-                    ]
-                )
-                row.append(max_center)
+            queue = []
+            for j in range(n):
+                queue.append(max(grid[i][j], grid[i + 1][j], grid[i + 2][j]))
+                if len(queue) >= 3:
+                    row.append(max(queue[-1], queue[-2], queue[-3]))
             maxLocal.append(row)
         return maxLocal
