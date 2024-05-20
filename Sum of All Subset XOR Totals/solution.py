a3378f5222e7073de615fa9ed63be7daa308f163
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2024-05-20 20:46:46 +0530

    add linear solution with bit manipulation

--- Sum of All Subset XOR Totals/solution.py
+++ Sum of All Subset XOR Totals/solution.py
@@ -1,8 +1,6 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        def dfs(i, total, nums):
-            if i >= len(nums):
-                return total
-            return dfs(i + 1, total ^ nums[i], nums) + dfs(i + 1, total, nums)
-
-        return dfs(0, 0, nums)
+        total = 0
+        for num in nums:
+            total |= num
+        return total << len(nums) - 1
