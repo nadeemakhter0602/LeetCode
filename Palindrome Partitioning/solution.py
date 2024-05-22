a3c1896e127b710b5a91170bc9111cab5db6c9da
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2024-05-22 23:13:29 +0530

    add Palindrome Partitioning

---
+++ Palindrome Partitioning/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def isPalindrome(s):
+            n = len(s)
+            if n == 1:
+                return True
+            for i in range(n // 2):
+                if s[i] != s[n - i - 1]:
+                    return False
+            return True
+
+        def allPalindromes(s, partition, palindromes):
+            if not s:
+                palindromes.append(partition)
+                return partition
+            for idx in range(len(s)):
+                if isPalindrome(s[: idx + 1]):
+                    allPalindromes(
+                        s[idx + 1 :], partition + [s[: idx + 1]], palindromes
+                    )
+            return partition
+
+        palindromes = []
+        allPalindromes(s, [], palindromes)
+        return palindromes
