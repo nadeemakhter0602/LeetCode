5c3cfb53eaa2fb0264f36f8114e20edbc39fde40
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2024-05-23 23:33:38 +0530

    palindrome partitioning use indices

--- Palindrome Partitioning/solution.py
+++ Palindrome Partitioning/solution.py
@@ -1,22 +1,22 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        def isPalindrome(s):
-            n = len(s)
-            if n == 1:
+        def isPalindrome(s, i, j):
+            if i == j:
                 return True
-            for i in range(n // 2):
-                if s[i] != s[n - i - 1]:
+            while i < j:
+                if s[i] != s[j]:
                     return False
+                i += 1
+                j -= 1
             return True
 
-        def allPalindromes(s, partition, palindromes):
-            if not s:
+        def allPalindromes(i, partition, palindromes):
+            if i >= len(s):
                 palindromes.append(partition)
-            for idx in range(len(s)):
-                if isPalindrome(s[: idx + 1]):
-                    allPalindromes(
-                        s[idx + 1 :], partition + [s[: idx + 1]], palindromes
-                    )
+                return palindromes
+            for j in range(i, len(s)):
+                if isPalindrome(s, i, j):
+                    allPalindromes(j + 1, partition + [s[i : j + 1]], palindromes)
             return palindromes
 
-        return allPalindromes(s, [], [])
+        return allPalindromes(0, [], [])
