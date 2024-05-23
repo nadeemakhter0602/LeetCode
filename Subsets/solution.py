2b15dd0031709b9193aeb037f36647027f3c2d71
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2024-05-23 23:37:05 +0530

    Subsets remove nums parameter

--- Subsets/solution.py
+++ Subsets/solution.py
@@ -1,12 +1,12 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def dfs(idx, subset, powerset, nums):
+        def dfs(idx, subset, powerset):
             if idx >= len(nums):
                 powerset.append(subset)
                 return powerset
-            dfs(idx + 1, subset, powerset, nums)
-            dfs(idx + 1, subset + [nums[idx]], powerset, nums)
+            dfs(idx + 1, subset, powerset)
+            dfs(idx + 1, subset + [nums[idx]], powerset)
             return powerset
 
-        powerset = dfs(0, [], [], nums)
+        powerset = dfs(0, [], [])
         return powerset
