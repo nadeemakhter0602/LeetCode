1a2c3ba4cd7f1e7616e552aa00965711fa6f54f3
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2024-06-05 20:17:27 +0530

    slight optimization

--- Find Common Characters/solution.py
+++ Find Common Characters/solution.py
@@ -1,19 +1,8 @@
-from collections import Counter
-
-
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        c = Counter(words[0])
-        for i in range(1, len(words)):
-            word = Counter(words[i])
-            keys = list(c.keys())
-            for l in keys:
-                if l in word:
-                    c[l] = min(c[l], word[l])
-                else:
-                    del c[l]
         res = []
-        for l in c:
-            for i in range(c[l]):
-                res.append(l)
+        letters = set(words[0])
+        for letter in letters:
+            frequency = min([word.count(letter) for word in words])
+            res += [letter] * frequency
         return res
