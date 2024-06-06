d3fa4b3b74636bc6c8f292409ebfab4884e1feab
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2024-06-06 18:58:59 +0530

    optimize solution

--- Hand of Straights/solution.py
+++ Hand of Straights/solution.py
@@ -1,16 +1,16 @@
+from collections import Counter
+
+
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         if len(hand) % groupSize != 0:
             return False
         hand.sort()
-        groups = [[] for i in range(len(hand) // groupSize)]
-        for i in range(len(hand)):
-            added = False
-            for group in groups:
-                if (not group or hand[i] - group[-1] == 1) and len(group) < groupSize:
-                    added = True
-                    group.append(hand[i])
-                    break
-            if not added:
-                return False
+        count = Counter(hand)
+        for i in hand:
+            if count[i] > 0:
+                for j in range(i, i + groupSize):
+                    count[j] = count.get(j, 0) - 1
+                    if count[j] < 0:
+                        return False
         return True
