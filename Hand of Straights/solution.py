f2794903998fba9f332542f8d073f3199ee0d716
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2024-06-06 22:27:38 +0530

    optimization - remove use of sorting

--- Hand of Straights/solution.py
+++ Hand of Straights/solution.py
@@ -5,9 +5,10 @@ class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         if len(hand) % groupSize != 0:
             return False
-        hand.sort()
         count = Counter(hand)
         for i in hand:
+            while count[i - 1]:
+                i -= 1
             if count[i] > 0:
                 for j in range(i, i + groupSize):
                     count[j] -= 1
