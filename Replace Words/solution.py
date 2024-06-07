381611ed1ba33bbe86ff5f229f870bd9b6eb777b
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2024-06-07 20:14:18 +0530

    slightly change approach, make code concise

--- Replace Words/solution.py
+++ Replace Words/solution.py
@@ -1,23 +1,16 @@
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
         dictionary = set(dictionary)
-        root = ""
-        res = ""
-        end = False
+        root, res, end = "", "", False
         for c in sentence:
             if c == " ":
-                if not end:
-                    res += root
-                end = False
-                root = ""
-                res += " "
+                res += root + " "
+                root, end = "", False
                 continue
             if end:
                 continue
             root += c
             if root in dictionary:
                 end = True
-                res += root
-        if not end:
-            res += root
+        res += root
         return res
