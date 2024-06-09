ffd54368e27e1cca16e58c0dace371743998da1b
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2024-06-09 12:19:17 +0530

    use list instead of dict, make code concise

--- Subarray Sums Divisible by K/solution.py
+++ Subarray Sums Divisible by K/solution.py
@@ -1,14 +1,11 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        s = 0
-        prefix = dict()
-        n = 0
+        s, n = 0, 0
+        prefix = [0 for i in range(k)]
+        prefix[0] = 1
         for i in range(len(nums)):
             s += nums[i]
             rem = s % k
-            if rem == 0:
-                n += 1
-            if rem in prefix:
-                n += prefix[rem]
-            prefix[rem] = 1 + prefix.get(rem, 0)
+            n += prefix[rem]
+            prefix[rem] += 1
         return n
