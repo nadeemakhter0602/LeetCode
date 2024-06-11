3c81ab7fce38af453e50775679e01ed34b092ccc
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2024-06-11 19:55:52 +0530

    use lambda functions to sort with hashmap

--- Relative Sort Array/solution.py
+++ Relative Sort Array/solution.py
@@ -1,17 +1,10 @@
-from collections import Counter
-
-
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        count = Counter(arr1)
-        res = []
-        for i in arr2:
-            for j in range(count[i]):
-                res.append(i)
-            del count[i]
-        sec = []
-        for i in count:
-            for j in range(count[i]):
-                sec.append(i)
-        sec.sort()
-        return res + sec
+        pos = dict()
+        for i in range(len(arr2)):
+            pos[arr2[i]] = i
+        for i in arr1:
+            if i not in pos:
+                pos[i] = 1000 + i
+        arr1.sort(key=lambda x: pos[x])
+        return arr1
