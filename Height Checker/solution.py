3658f233f2c539dd17d7c90ceb9eb85c8f66c5b9
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2024-06-11 19:43:14 +0530

    Height Checker use counting sort

--- Height Checker/solution.py
+++ Height Checker/solution.py
@@ -1,3 +1,17 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        return sum([i != j for i, j in zip(sorted(heights), heights)])
+        max_height = max(heights) + 1
+        expected = [0 for i in range(max_height)]
+        for i in heights:
+            expected[i] += 1
+        idx = 0
+        n = 0
+        for i in range(1, max_height):
+            if expected[i] == 0:
+                continue
+            while expected[i] > 0:
+                if heights[idx] != i:
+                    n += 1
+                expected[i] -= 1
+                idx += 1
+        return n
