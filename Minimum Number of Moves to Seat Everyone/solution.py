b003990f2626627c62f3bbf348748ddc74a96138
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2024-06-13 21:17:25 +0530

    use counting sort array

--- Minimum Number of Moves to Seat Everyone/solution.py
+++ Minimum Number of Moves to Seat Everyone/solution.py
@@ -1,8 +1,14 @@
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
-        seats.sort()
-        students.sort()
-        res = 0
-        for i, j in zip(seats, students):
-            res += abs(i - j)
-        return res
+        n = max(max(seats), max(students))
+        differences = [0 for i in range(n)]
+        for seat in seats:
+            differences[seat - 1] += 1
+        for student in students:
+            differences[student - 1] -= 1
+        moves = 0
+        unmatched = 0
+        for difference in differences:
+            moves += abs(unmatched)
+            unmatched += difference
+        return moves
